07af79f0e30627fc0def72efb1885fc5d876c8f6
Lucas-LM001 <Lucas.ms1999@aluno.ifsc.edu.br>
2024-12-20 00:08:16 +0000

    Comecando o Scan e Nao bater parede

--- yourrobot.java
+++ yourrobot.java
@@ -20,5 +20,24 @@ public class AimLock extends AdvancedRobot {
         turnRadarRightRadians(Double.POSITIVE_INFINITY); // Faz o radar girar continuamente para a direita
 
     }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double absBearing = e.getBearingRadians() + getHeadingRadians(); // Calcula o ângulo do inimigo
+        double latVel = e.getVelocity() * Math.sin(e.getHeadingRadians() - absBearing); // Velocidade do inimigo
+        double gunTurnAmt; // Quantidade que a arma precisa girar
+        setTurnRadarLeftRadians(getRadarTurnRemainingRadians()); // Trava o radar no inimigo
+
+        // Chance aleatória de mudar a velocidade para confundir o inimigo
+        if (Math.random() > 0.9) {
+            setMaxVelocity((12 * Math.random()) + 12); // Define uma nova velocidade máxima aleatória
+        }
+
+
+
+    }
+
+    public void onHitWall(HitWallEvent e) {
+        moveDirection = -moveDirection; // Inverte a direção ao bater em uma parede
+    }
     
 }
